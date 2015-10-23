3c44590d4962a8ffa8455a8c99fbb24fb9ded314
AMAN SRIVASTAVA <mailbox@amansrivastava.in>
2015-10-23 18:49:08 +0530

    Create Stock.php
    Overwrite default Stock ID with store ID

---
+++ app/code/community/Credevlabz/EasyWarehouse/Model/CatalogInventory/Stock.php
@@ -0,0 +1,23 @@
+<?php
+class Credevlabz_EasyWarehouse_Model_CatalogInventory_Stock extends Mage_CatalogInventory_Model_Stock
+{
+	public function getId(){
+		return $this->getStoreId();
+	}
+	
+	/**
+     * Retrieve Store Id (product or current)
+     *
+     * @return int
+     */
+    public function getStoreId()
+    {
+        $storeId = $this->getData('store_id');
+        if (is_null($storeId)) {
+            $storeId = Mage::app()->getStore()->getId();
+            $this->setData('store_id', $storeId);
+        }
+        return $this;
+    }
+}
+		
